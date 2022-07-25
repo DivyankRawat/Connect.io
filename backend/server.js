4b8e2bc45799c2fe4f557fedf413594ab95f60ed
Divyank <divyankrawat10@gmail.com>
2022-07-25 22:52:19 +0530

    -

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,21 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const chats = require('./data/dummyData')
 
-app.listen(5000, console.log('server is running on port 5000'))
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const chatById = chats.find((chat) => chat._id === req.params.id);
+    res.send(chatById);
+})
+
+app.listen(process.env.PORT || 5000, console.log(`server is running on ${process.env.port || 5000}`))
