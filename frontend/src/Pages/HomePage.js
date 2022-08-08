b9a96caf2dbe1bd8ee7e37299baa50405192d97a
Divyank <divyankrawat10@gmail.com>
2022-08-08 10:04:56 +0530

    -

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import '../App.css'
+import {
+    Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+} from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className='home'>
+            <Container display='flex' marginTop='35px' maxW="xl" centerContent>
+                <Box bg="#121212" w="100%" p={4} color='white' borderRadius="xl" boxShadow='0px 0px 10px #7e35c7' border='1px solid #d000fe'>
+                    <Text fontSize="6xl" marginBottom='10px' color='white' fontFamily="Lobster" d="flex"
+                        justifyContent="center" textAlign='center'>
+                        Connect.io
+                    </Text>
+                    <Tabs isFitted variant="soft-rounded" colorScheme='purple'>
+                        <TabList mb="1em">
+                            <Tab>Login</Tab>
+                            <Tab>Sign Up</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <Login />
+                            </TabPanel>
+                            <TabPanel>
+                                <Signup />
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </Box>
+            </Container >
+        </div>
     )
 }
 
---
+++ frontend/src/components/Authentication/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import axios from "axios";
+
+const Login = () => {
+    const [show, setShow] = useState(false);
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
+
+    const handleClick = () => setShow(!show);
+
+    return (
+        <div> <VStack spacing="10px">
+            <FormControl id="email" isRequired>
+                <FormLabel>Email Address</FormLabel>
+                <Input
+                    value={email}
+                    type="email"
+                    placeholder="Enter Your Email Address"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </FormControl>
+            <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup size="md">
+                    <Input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        type={show ? "text" : "password"}
+                        placeholder="Enter password"
+                    />
+                    <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" color='black' onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+            </FormControl>
+            <Button
+                colorScheme="purple"
+                width="100%"
+                style={{ marginTop: 15 }}
+                // onClick={submitHandler}
+                isLoading={loading}
+            >
+                Login
+            </Button>
+            <Button
+                variant="solid"
+                colorScheme="white"
+                width="100%"
+                onClick={() => {
+                    setEmail("guest@example.com");
+                    setPassword("123456");
+                }}
+            >
+                Get Guest User Credentials
+            </Button>
+        </VStack></div>
+    )
+}
+
+export default Login
